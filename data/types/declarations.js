2fbdddbd8a312991e31761f710be9046b6f04b0d
theusaf <theusafyt@gmail.com>
2021-07-30 14:16:28 -0700

    Add descriptions of declarations

--- data/types/declarations.js
+++ data/types/declarations.js
@@ -4,11 +4,17 @@ module.exports = [
   new Declaration("class", "class")
     .setSnippet(`class \${1:Name} {
   $2
-}`),
+}`)
+    .setDescription("Create a class."),
   new Declaration("clext", "class...extends")
     .setSnippet(`class \${1:Name} extends \${2:Parent} {
   $3
-}`),
-  new Declaration("global", "global"),
+}`)
+    .setDescription("Create a class which extends another class"),
+  new Declaration("global", "global")
+    .setDescription("To refer to an existing global variable inside a function (or create a new one), declare the variable as global prior to using it."),
   new Declaration("static", "static")
+    .setDescription("Static variables are always implicitly local, but differ from locals because their values are remembered between calls."),
+  new Declaration("extends", "extends")
+    .setDescription("Keyword to extend classes from other classes.")
 ];
